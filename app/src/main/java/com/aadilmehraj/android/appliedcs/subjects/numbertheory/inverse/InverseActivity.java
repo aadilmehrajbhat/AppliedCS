e2d10734e13c3e6ffa9dcded36c1e7a902a51e50
Aadil Mehraj <aadilMehraj76@gmail.com>
2018-04-04 20:13:34 +0530

    0.2 - [NumberTheory] Inverse initial commit

---
+++ app/src/main/java/com/aadilmehraj/android/appliedcs/subjects/numbertheory/inverse/InverseActivity.java
@@ -0,0 +1,14 @@
+package com.aadilmehraj.android.appliedcs.subjects.numbertheory.inverse;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.aadilmehraj.android.appliedcs.R;
+
+public class InverseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inverse);
+    }
+}
